a278bd844a07bf2edf4c7022f6c01a2686a7a590
Kamyyylo <kamyszek1@gmail.com>
2020-04-26 18:33:10 +0200

    Tests work now

--- processorApp/src/test/java/yourmedicines/processor/Services/MedicineHeadServiceTest.java
+++ processorApp/src/test/java/yourmedicines/processor/Services/MedicineHeadServiceTest.java
@@ -5,49 +5,40 @@ import static org.mockito.Mockito.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import yourmedicines.processor.domain.medicineleaflet.MedicineHead;
 import yourmedicines.processor.domain.medicineleaflet.MedicineTypes;
 import yourmedicines.processor.repositories.medicineleaflet.MedicineHeadRepository;
 import yourmedicines.processor.services.medicineleaflet.MedicineHeadService;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
 public class MedicineHeadServiceTest {
+
+    @InjectMocks
+    private MedicineHeadService medicineHeadService;
 
     @Mock
     MedicineHeadRepository medicineHeadRepositoryMock;
-
-    @Mock
-    MedicineHead medicineHeadTest;
-
-    @Autowired
-    private MedicineHeadService medicineHeadService;
 
     @Test
     public void buildMedicineId_if_not_in_db_Test() {
         MedicineHead testObj = new MedicineHead();
         testObj.setMedicineName("medicine");
         testObj.setMedicineType(MedicineTypes.PILL);
-        when(medicineHeadRepositoryMock.getByMedicineId(any())).thenReturn(isNull());
+        when(medicineHeadRepositoryMock.getByMedicineId(anyString())).thenReturn(null);
         String medicineId = medicineHeadService.buildMedicineId(testObj);
         assertEquals("MEDICINE_PILL_0", medicineId);
     }
 
     @Test
     public void buildMedicineId_if_exist_in_db_Test() {
-        MedicineHead testObj = new MedicineHead();
-        testObj.setMedicineName("medicine");
-        testObj.setMedicineType(MedicineTypes.PILL);
-        StringBuilder sb = new StringBuilder();
-        when(sb.toString()).thenReturn("");
-        when(medicineHeadRepositoryMock.getByMedicineId(anyString())).thenReturn(isNotNull());
-
-        //when(medicineHeadRepositoryMock.getByMedicineId("MEDICINE_PILL_0")).thenReturn(isNull());
-        String medicineId = medicineHeadService.buildMedicineId(testObj);
-        assertEquals("MEDICINE_PILL_0", medicineId);
+        MedicineHead medicineHead = new MedicineHead();
+        medicineHead.setMedicineName("medicine");
+        medicineHead.setMedicineType(MedicineTypes.PILL);
+        when(medicineHeadRepositoryMock.getByMedicineId("MEDICINE_PILL_0")).thenReturn(medicineHead);
+        String medicineId = medicineHeadService.buildMedicineId(medicineHead);
+        assertEquals("MEDICINE_PILL_1", medicineId);
     }
 }
